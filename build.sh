95a257419e2b4363d608ef72db03883cb3a1e772
sansar28v-cmyk <sansar28v@gmail.com>
2026-03-14 11:59:57 +0530

    Add build.sh script for Railway deployment

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "Installing client dependencies..."
+cd client && npm install
+
+echo "Building client..."
+npm run build
+
+echo "Installing server dependencies..."
+cd ../server && npm install
+
+echo "Build complete!"
